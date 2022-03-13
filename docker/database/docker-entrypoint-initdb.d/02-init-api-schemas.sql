f6b2b449ea26d3a8a3d8b20e4088673050a0fa57
fabien_renaud <frd.frenaud@gmail.com>
2022-03-13 23:28:13 +0100

    feat: expose simple resources

--- docker/database/docker-entrypoint-initdb.d/02-init-api-schemas.sql
+++ docker/database/docker-entrypoint-initdb.d/02-init-api-schemas.sql
@@ -1,24 +1,45 @@
 CREATE SCHEMA ubeer_api;
 
 CREATE OR REPLACE VIEW ubeer_api.beers AS
-SELECT beer.id         AS "id",
-       beer.name       AS "name",
-       beer.abv        AS "abv",
-       beer.ibu        AS "ibu",
-       beer.created_at AS "createdAt",
-       beer.updated_at AS "updatedAt",
-       beer.deleted_at AS "deletedAt",
-       JSONB_STRIP_NULLS(
-               JSONB_BUILD_OBJECT(
-                       'id', brewery.id,
-                       'name', brewery.name,
-                       'address', COALESCE(brewery.address),
-                       'contact', brewery.contact,
-                       'email', brewery.email,
-                       'createdAt', beer.created_at,
-                       'updatedAt', beer.updated_at,
-                       'deletedAt', beer.deleted_at
-                   )) AS brewery
-FROM ubeer.beer,
-     ubeer.brewery
-WHERE ubeer.beer.brewery__id = ubeer.brewery.id
+SELECT id         AS "id",
+       name       AS "name",
+       abv        AS "abv",
+       ibu        AS "ibu",
+       created_at AS "createdAt",
+       updated_at AS "updatedAt",
+       deleted_at AS "deletedAt"
+FROM ubeer.beer;
+
+CREATE OR REPLACE VIEW ubeer_api.breweries AS
+SELECT id         AS "id",
+       name       AS "name",
+       address    AS "address",
+       contact    AS "contact",
+       email      AS "email",
+       created_at AS "createdAt",
+       updated_at AS "updatedAt",
+       deleted_at AS "deletedAt"
+FROM ubeer.brewery;
+
+-- CREATE OR REPLACE VIEW ubeer_api.beers AS
+-- SELECT beer.id         AS "id",
+--        beer.name       AS "name",
+--        beer.abv        AS "abv",
+--        beer.ibu        AS "ibu",
+--        beer.created_at AS "createdAt",
+--        beer.updated_at AS "updatedAt",
+--        beer.deleted_at AS "deletedAt",
+--        JSONB_STRIP_NULLS(
+--                JSONB_BUILD_OBJECT(
+--                        'id', brewery.id,
+--                        'name', brewery.name,
+--                        'address', COALESCE(brewery.address),
+--                        'contact', brewery.contact,
+--                        'email', brewery.email,
+--                        'createdAt', beer.created_at,
+--                        'updatedAt', beer.updated_at,
+--                        'deletedAt', beer.deleted_at
+--                    )) AS brewery
+-- FROM ubeer.beer,
+--      ubeer.brewery
+-- WHERE ubeer.beer.brewery__id = ubeer.brewery.id
